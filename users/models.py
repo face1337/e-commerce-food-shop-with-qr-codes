f99b5255f513a28cfe0831ef45d4c2646312adaf
face1337 <facebaster@gmail.com>
2020-11-10 11:14:11 +0100

    Utworzony podstawowy model Profile -> będzie rozwijany

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image, ImageDraw
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField("Adres:", max_length=255)
+    qr_code = models.ImageField(upload_to='qr_codes', blank=True)
