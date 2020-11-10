1d01dd3a6c93e1acb382b00cbe83bde7290423af
face1337 <facebaster@gmail.com>
2020-11-10 14:29:18 +0100

    Dodanie __str__ do modelu Profil w users

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+import qr_code
+from io import BytesIO
+from django.core.files import File
 from PIL import Image, ImageDraw
 
 
@@ -7,3 +10,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField("Adres:", max_length=255)
     qr_code = models.ImageField(upload_to='qr_codes', blank=True)
+
+    def __str__(self):
+        return 'Profil użytkownika: {}'.format(self.name)
+
+    #TODO: save method for generating a qr code based on given address
+
