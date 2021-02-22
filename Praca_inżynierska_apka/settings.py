7c23d373429906349b9262ed77268576f276763a
Tomasz Wańczyk <facebaster@gmail.com>
2021-02-22 12:59:48 +0100

    Update settings.py

--- Praca_inżynierska_apka/settings.py
+++ Praca_inżynierska_apka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wuanm@oq@=b7463m=tns18t-$v)=4-u)4k$06j1&&%1_wq+on4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'Praca_inżynierska_apka.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'student_zamawia',
-        'USER': 'postgres',
-        'PASSWORD': '123Qweasd!',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
